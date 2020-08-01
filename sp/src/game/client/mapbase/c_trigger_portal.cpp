9b99929f7726beda00e1abf2727273d8a8e6da37
FriskTheFallenHuman <nikodash2020@hotmail.com>
2020-08-01 18:33:01 -0500

    Create clientside version of trigger portal
    apparently the fix provide in the valve wiki seems to be outdate :expressionless:

---
+++ sp/src/game/client/mapbase/c_trigger_portal.cpp
@@ -0,0 +1,31 @@
+//========= Copyright Valve Corporation, All rights reserved. ============//
+//
+// Purpose: Entity which teleports touched entities and reorients their physics
+//
+//=============================================================================//
+
+#include "cbase.h"
+
+// memdbgon must be the last include file in a .cpp file!!!
+#include "tier0/memdbgon.h"
+
+//////////////////////////////////////////////////////////////////////////
+// C_TriggerPortal
+// Moves touched entity to a target location, changing the model's orientation
+// to match the exit target. It differs from CTriggerTeleport in that it
+// reorients physics and has inputs to enable/disable its function.
+//////////////////////////////////////////////////////////////////////////
+class C_TriggerPortal : public C_BaseEntity
+{
+public:
+	DECLARE_CLASS( C_TriggerPortal, C_BaseEntity );
+	DECLARE_CLIENTCLASS();
+
+private:
+	C_TriggerPortal* m_hRemotePortal;
+
+};
+
+IMPLEMENT_CLIENTCLASS_DT( C_TriggerPortal, DT_TriggerPortal, CTriggerPortal )
+	RecvPropBool( RECVINFO( m_hRemotePortal ) ),
+END_RECV_TABLE()
