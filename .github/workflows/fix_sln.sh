664b2f288fd7a12ec95761c9860d6d288747651e
Blixibon <acj3017@gmail.com>
2022-10-26 07:39:11 -0500

    cat solution

--- .github/workflows/fix_sln.sh
+++ .github/workflows/fix_sln.sh
@@ -14,6 +14,20 @@ Global
 		Debug|Win32 = Debug|Win32
 		Release|Win32 = Release|Win32
 	EndGlobalSection
+	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+EOF
+while read -r line; do
+	[ "${line:0:8}" != "Project(" ] && continue
+	id="$(echo "${line}"|cut -d',' -f 3)"
+	id="${id:2:-2}"
+	for cfg in "Debug" "Release"; do
+		for param in "ActiveCfg" "Build.0"; do
+			echo "		${id}.${cfg}|Win32.${param} = ${cfg}|Win32"
+		done
+	done
+done | crlf
+crlf <<EOF
+	EndGlobalSection
 	GlobalSection(SolutionProperties) = preSolution
 		HideSolutionNode = FALSE
 	EndGlobalSection
