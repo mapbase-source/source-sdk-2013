1cf4536002be9f60317047b70bbd7805310a604e
Sectonidse <94748461+Sectonidse@users.noreply.github.com>
2024-07-22 22:30:39 +0300

    Update view.cpp

--- sp/src/game/client/view.cpp
+++ sp/src/game/client/view.cpp
@@ -119,7 +119,7 @@ ConVar mat_viewportscale( "mat_viewportscale", "1.0", FCVAR_ARCHIVE, "Scale down
 ConVar mat_viewportupscale( "mat_viewportupscale", "1", FCVAR_ARCHIVE, "Scale the viewport back up" );
 ConVar cl_leveloverview( "cl_leveloverview", "0", FCVAR_CHEAT );
 
-static ConVar r_mapextents( "r_mapextents", "16384", FCVAR_CHEAT, 
+static ConVar r_mapextents( "r_mapextents", "32768", FCVAR_CHEAT, 
 						   "Set the max dimension for the map.  This determines the far clipping plane" );
 
 // UNDONE: Delete this or move to the material system?
