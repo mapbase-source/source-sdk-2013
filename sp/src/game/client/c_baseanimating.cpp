4794683a2cfd0d5c11ccf3c1431105a5e76c3d39
Kerim Čamdžić <hl2.for.you@gmail.com>
2025-04-04 14:54:21 +0200

    Fix deathposes being non-functional using ValveSoftware/source-sdk-2013#796

--- sp/src/game/client/c_baseanimating.cpp
+++ sp/src/game/client/c_baseanimating.cpp
@@ -2014,7 +2014,11 @@ void C_BaseAnimating::MaintainSequenceTransitions( IBoneSetup &boneSetup, float 
 	if ( !boneSetup.GetStudioHdr() )
 		return;
 
-	if ( prediction->InPrediction() )
+	if ( prediction->InPrediction()
+#ifdef FP
+		|| IsAboutToRagdoll()
+#endif // FP
+		)
 	{
 		m_nPrevNewSequenceParity = m_nNewSequenceParity;
 		return;
