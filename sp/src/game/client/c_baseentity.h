9b8635954c4ac82f10fa87b3824d6a5c29d4c8c1
Zeldaboy14 <zeldaboy1999@gmail.com>
2026-07-08 03:39:23 -0500

    [feature] beam_spotlight support
    entity from the l4d engine branch. was in alien swarm, but not ported to mapbase yet

--- sp/src/game/client/c_baseentity.h
+++ sp/src/game/client/c_baseentity.h
@@ -2318,6 +2318,14 @@ inline bool C_BaseEntity::IsEnabledInToolView() const
 }
 
 //-----------------------------------------------------------------------------
+// Client version of UTIL_Remove
+//-----------------------------------------------------------------------------
+inline void UTIL_Remove(C_BaseEntity* pEntity)
+{
+    pEntity->Remove();
+}
+
+//-----------------------------------------------------------------------------
 // Purpose: 
 // Input  :  - 
 // Output : inline bool
